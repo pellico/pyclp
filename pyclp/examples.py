8583fab47b7b2da7ae59f813aca6a1f3b717e8ca
pellico <pellico@users.noreply.github.com>
2023-07-15 15:31:49 +0200

    Added an example for cut

--- pyclp/examples.py
+++ pyclp/examples.py
@@ -16,41 +16,41 @@
 #OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
 #THE POSSIBILITY OF SUCH DAMAGE.
 
-import pyclp
+from pyclp import *
 
 def example1():
     """Simple unification
     """
-    pyclp.init()                        #Init ECLiPSe engine
-    myVar=pyclp.Var()                   #Create a prolog variable
-    my_goal=pyclp.Compound("=",myVar,1) # Create goal
+    init()                        #Init ECLiPSe engine
+    myVar=Var()                   #Create a prolog variable
+    my_goal=Compound("=",myVar,1) # Create goal
     my_goal.post_goal()                 #Post goal
-    result,dummy =pyclp.resume()        #Resume execution of ECLiPSe engine
-    if result != pyclp.SUCCEED:
+    result,dummy =resume()        #Resume execution of ECLiPSe engine
+    if result != SUCCEED:
         raise
     print(myVar.value() +1)               #myVar.value get the var value back.
     print(myVar)                        #print value
-    pyclp.cleanup()                     #Shutdown ECLiPSe engine
+    cleanup()                     #Shutdown ECLiPSe engine
     
     
 
 def example2():
     """Using streams
     """
-    pyclp.init()
-    goal1=pyclp.Compound('writeln','Hello world')
+    init()
+    goal1=Compound('writeln','Hello world')
     goal1.post_goal()
-    result,stream_num=pyclp.resume()
-    if result==pyclp.FLUSHIO:
-        output_stream=pyclp.Stream(stream_num)
+    result,stream_num=resume()
+    if result==FLUSHIO:
+        output_stream=Stream(stream_num)
         print(output_stream.readall())
         output_stream.close()
     else:
         raise
-    result,dummy=pyclp.resume()
-    if result != pyclp.SUCCEED:
+    result,dummy=resume()
+    if result != SUCCEED:
         raise
-    pyclp.cleanup()
+    cleanup()
     
     
 def example3():
@@ -58,23 +58,46 @@ def example3():
     Search example using ic library
     """
     print("Search example")
-    pyclp.init()                                         # Init ECLiPSe engine
-    pyclp.Compound("lib",pyclp.Atom("ic")).post_goal()   # Load ic library
-    A_var=pyclp.Var()                                    # Create variable A
-    B_var=pyclp.Var()                                    # Create variable B
-    pyclp.Compound("#::",pyclp.PList([A_var,B_var]),pyclp.Compound("..",1,10)).post_goal() # [A,B]#::1..10
-    pyclp.Compound("#<",A_var,B_var).post_goal()         # A#<B
-    pyclp.Compound("#=",A_var,5).post_goal()             # A#=5
-    pyclp.Compound("labeling",pyclp.PList([A_var,B_var])).post_goal()  # labeling([A,B])
+    init()                                         # Init ECLiPSe engine
+    Compound("lib",Atom("ic")).post_goal()   # Load ic library
+    A_var=Var()                                    # Create variable A
+    B_var=Var()                                    # Create variable B
+    Compound("#::",PList([A_var,B_var]),Compound("..",1,10)).post_goal() # [A,B]#::1..10
+    Compound("#<",A_var,B_var).post_goal()         # A#<B
+    Compound("#=",A_var,5).post_goal()             # A#=5
+    Compound("labeling",PList([A_var,B_var])).post_goal()  # labeling([A,B])
     # Loop on all solution and print them.
-    while (pyclp.resume()[0]==pyclp.SUCCEED):
+    while (resume()[0]==SUCCEED):
         print(B_var)
-        pyclp.Atom("fail").post_goal()                   # Post fail for backtracking over solutions
+        Atom("fail").post_goal()                   # Post fail for backtracking over solutions
          
-    pyclp.cleanup()                                      #Shutdown ECLiPSe engine
-    
+    cleanup()                                      #Shutdown ECLiPSe engine
+
+
+def example4():
+    """
+    Cut example using ic library
+    """
+    print("Cut Example")
+    init() # Init ECLiPSe engine
+    Compound("lib","lists").post_goal()                 # lib(lists)
+    A_var=Var()                                         # Create variable A
+    Compound("member",A_var,PList([1,2,3])).post_goal() # member(A_var,[1,2,3])
+    # Loop on all solution and print them.
+    while (resume()[0]==SUCCEED):                       
+        if A_var.value()==2:                            # When value == 2 cut all other solutions.
+            cut()
+            break
+        Atom("fail").post_goal()                        # Post fail for backtracking over solutions
+    B_var=Var()                                         # Create variable B
+    Compound("is",B_var,\
+             Compound("+",A_var,1)).post_goal()         # B_Var is A_var + 1
+    resume()
+    print(A_var,B_var)
+    cleanup()                                           #Shutdown ECLiPSe engine    
 
 if __name__ == '__main__':
     example1()
     example2()
     example3()
+    example4()
