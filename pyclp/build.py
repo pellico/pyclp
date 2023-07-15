80ee20c53bc85213d5db343e4345cbc44ae2f184
pellico <pellico@users.noreply.github.com>
2023-07-15 15:32:39 +0200

    Use poetry to build extension

--- pyclp/build.py
+++ pyclp/build.py
@@ -1,30 +1,9 @@
-#!/usr/bin/env python
-
-#Simplified BSD License
-#Copyright (c) 2012, Oreste Bernardi
-#All rights reserved.
-#
-#Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-#
-#    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-#    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
-#    in the documentation and/or other materials provided with the distribution.
-#
-#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-#BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-#IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
-#OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-#OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
-#THE POSSIBILITY OF SUCH DAMAGE.
-"""
-setup.py file for pyclp
-"""
-
-import distutils
-from distutils.core import setup
-from distutils.extension import Extension
+import setuptools
+from setuptools.extension import Extension
 import platform
+# import sys
+# cython and python dependency is handled by pyproject.toml
+from Cython.Build import cythonize
 
 class exceptionPyClpSetup(Exception):
     def __init__(self,msg):
@@ -37,7 +16,7 @@ from os import environ
 import os.path
 from types import MethodType
 
-class build_ext_subclass( build_ext ):
+class BuildExt( build_ext ):
     def build_extensions(self):
         # Dirty trick to generate the required eclipse.lib file before linking
         # At this stage the path to toolchain is configured
@@ -94,6 +73,9 @@ else:
 eclise_include_path=os.path.join(eclipsedir,'include',arch)
 eclipse_lib_path=os.path.join(eclipsedir,'lib',arch)
 
+
+# Compilation of C module in c_lib
+
 pyclp_module = Extension('pyclp.pyclp',
                            ['src/pyclp/pyclp.pyx'],
                            library_dirs=[eclipse_lib_path],
@@ -101,26 +83,13 @@ pyclp_module = Extension('pyclp.pyclp',
                            include_dirs=[eclise_include_path,'src/pyclp/']
                            )
 
-with open('README.txt') as file:
-    long_description_read = file.read()
 
-setup (name = 'PyCLP',
-       version = '1.1',
-       author      = "Oreste Bernardi",
-       maintainer = "Oreste Bernardi",
-       author_email= "<name>@<name>.eu  name=oreste",
-       url = "https://sourceforge.net/projects/pyclp/",
-       description = """Interface to ECLiPSe CLP""",
-       license= "Simplified BSD",
-       cmdclass = {'build_ext': build_ext_subclass},
-       ext_modules = [pyclp_module],
-       package_dir={'': 'src'},
-       packages=['pyclp'],
-       long_description=long_description_read,
-       classifiers=["Development Status :: 5 - Production/Stable",
-                    "Programming Language :: Prolog",
-                    "License :: OSI Approved :: BSD License",
-                    "Programming Language :: Cython" 
-                    ]
-       #py_modules = ["pyclp"],
-       )
+
+def build(setup_kwargs):
+    setup_kwargs.update(
+        dict(
+            cmdclass=dict(build_ext=BuildExt),
+            ext_modules=[pyclp_module],
+            #zip_safe=False
+        )
+    )
